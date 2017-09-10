b3d0e3baaf760fd1f93f9f3e180af290523fb8a6
KentShepard <kentshepard7@gmail.com>
2017-09-10 13:22:31 -0400

    Set up structure of each view with the sample data

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,10 +1,17 @@
 var VideoListView = Backbone.View.extend({
 
-
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+
+    this.collection.each(this.renderVideo, this);
+
     return this;
+  },
+
+  renderVideo: function(video) {
+    var videoListEntryView = new VideoListEntryView({model: video});
+    this.$('.video-list').append(videoListEntryView.render().el);
   },
 
   template: templateURL('src/templates/videoList.html')
